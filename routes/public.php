99ffe80cc4005e0f5919856ecbc6704efb51056c
meeglos <miguel@rodriguez.me>
2017-02-04 00:07:32 +0100

    Show post details

--- routes/public.php
+++ routes/public.php
@@ -11,6 +11,7 @@
 |
 */
 
+//use App\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('posts/{post}', [
+    'as' => 'posts.show',
+    'uses' => 'PostController@show'
+])->where('post', '[0-9]+'); // '\d+'  
---
+++ tests/features/ShowPostTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Post;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ShowPostTest extends TestCase
+{
+    function test_a_user_can_see_the_post_details()
+    {
+        $user = $this->defaultUser([
+            'name' => 'Miguel Rodriguez',
+        ]);
+
+        $post = factory(Post::class)->make([
+            'title' => 'Este es el titulo del post',
+            'content' => 'Este es el contenido del post',
+        ]);
+
+        $user->posts()->save($post);
+
+//        When
+        $this->visit(route('posts.show', $post))
+            ->seeInElement('h1', $post->title)
+            ->see($post->content)
+            ->see($user->name);
+    }
+}
