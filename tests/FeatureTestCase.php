ebe79ecaf4186e09678bcf26f29628a3314eb7bd
meeglos <miguel@rodriguez.me>
2017-02-03 01:36:44 +0100

    Final test environment

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,13 @@
+<?php
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * Created by PhpStorm.
+ * User: miguel
+ * Date: 03/02/2017
+ * Time: 1:20
+ */
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+}
