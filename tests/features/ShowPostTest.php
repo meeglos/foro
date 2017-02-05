84adee2b095bb6796eed190280c10ce1947bc2bc
meeglos <miguel@rodriguez.me>
2017-02-05 22:29:47 +0100

    Regretion testing

--- tests/features/ShowPostTest.php
+++ tests/features/ShowPostTest.php
@@ -20,10 +20,30 @@ class ShowPostTest extends TestCase
 
         $user->posts()->save($post);
 
+//        dd($post->url);
+
 //        When
-        $this->visit(route('posts.show', $post))
+        $this->visit($post->url)
             ->seeInElement('h1', $post->title)
             ->see($post->content)
             ->see($user->name);
+    }
+
+    function test_old_urls_are_redirected()
+    {
+        $user = $this->defaultUser();
+
+        $post = factory(Post::class)->make([
+            'title' => 'Old title',
+        ]);
+
+        $user->posts()->save($post);
+
+        $url = $post->url;
+
+        $post->update(['title' => 'New title']);
+
+        $this->visit($url)
+            ->seePageIs($post->url);
     }
 }
